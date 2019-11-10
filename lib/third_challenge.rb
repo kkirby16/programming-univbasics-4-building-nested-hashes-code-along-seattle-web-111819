fa4783c4b8a8de5879c2148366e2e50e0b4fe76c
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-10 04:58:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        :age
         },
       :matriarch => {
         #your key/value pairs here
